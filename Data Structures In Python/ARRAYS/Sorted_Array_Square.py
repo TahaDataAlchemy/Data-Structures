2ba3a73a18d804cda41d5061ed7bc5458ed52fec
TahaDataAlchemy <tahamehboob281@gmail.com>
2024-06-04 14:58:51 +0500

    OPTIMIZE LOGIC

--- Data Structures In Python/ARRAYS/Sorted_Array_Square.py
+++ Data Structures In Python/ARRAYS/Sorted_Array_Square.py
@@ -23,3 +23,27 @@ if __name__ == "__main__":
   my_list = [-4, -1, 0, 3, 10]  # Use list instead of 'list' (reserved keyword)
   print(sorted_squared_array(my_list))
 
+
+##Another Method
+def Optimize_solution(myArray):
+  if len(myArray) == 0:
+    return []
+  
+  left = 0
+  right = len(myArray) - 1
+  squared_Array = []
+  while left <= right:
+    a = myArray[left] ** 2
+    b = myArray[right] ** 2
+    if a < b:
+      squared_Array.append(a)
+      left += 1
+    else:
+      squared_Array.append(b)
+      right -= 1
+
+  return sorted(squared_Array)
+
+if __name__ == "__main__":
+  listed = [-3, 2, -9, 4, -10]
+  print(Optimize_solution(listed))
